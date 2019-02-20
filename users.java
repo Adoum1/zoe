9312def1f508eb9dd7d27e703ab366f2705cc270
Emilien Merger <emilien.merger@viacesi.fr>
2019-02-20 14:50:19 +0100

    Add some users data for mangodb

---
+++ users.java
@@ -0,0 +1,18 @@
+{
+"lastName" : "Dupond",
+"firstName" : "Alexandre",
+"password" : "password",
+"userPrivilege" : "guest"				
+}
+{
+"lastName" : "Lechaine",
+"firstName" : "Amelie",
+"password" : "password",
+"userPrivilege" :"guest"				
+}
+{
+"lastName" : "Deschamps",
+"firstName" : "Julien",
+"password" : "password",
+"userPrivilege" : "guest"				
+}
